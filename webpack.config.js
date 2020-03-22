7bd662fc1581a4abab9e414dd1853421229fa8f7
Munch_TZB <www.tzb@gmail.com>
2020-03-22 12:58:23 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -23,6 +24,11 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true
+    })
+  ]
 };
 
